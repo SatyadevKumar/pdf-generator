8251ba9e23d5ebd1a2bff91d60db7f39562d09a5
SatyadevKumar <satyadev1986@gmail.com>
2023-10-01 16:54:59 -0500

    repo

---
+++ Satya/server.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const puppeteer = require('puppeteer');
+const path = require('path');
+const axios = require('axios');
+const Handlebars = require('handlebars');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+// Serve static HTML file for testing
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+app.get('/PDFTemplate.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'PDFTemplate.html'));
+});
+
+app.post('/generate-pdf', async (req, res) => {
+  try {
+    const data = req.body;
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const response = await axios.get("http://localhost:3000/PDFTemplate.html");
+    const templateContent = response.data;
+    const renderedContent = Handlebars.compile(templateContent)(data);
+    await page.setContent(renderedContent);
+
+    // Create a chart using Chart.js
+    await page.evaluate((data) => {
+      const ctx = document.getElementById('chart').getContext('2d');
+      console.log(data);
+      const chartData = {
+        labels: ['Equity Amount', 'US Equity', 'Non-US Equity'],
+        datasets: [
+          {
+            label: 'Asset Allocation',
+            backgroundColor: [
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+            ],
+            borderColor: [
+              'rgba(75, 192, 192, 1)',
+              'rgba(255, 99, 132, 1)',
+              'rgba(255, 206, 86, 1)',
+            ],
+            borderWidth: 1,
+            data: [
+              data['assetAllocation'][0].equityAmount,
+              data['assetAllocation'][0].usEquity,
+              data['assetAllocation'][0].nonUsEquity,
+            ],
+          },
+        ],
+      };
+
+      const chartConfig = {
+        type: 'bar',
+        data: chartData,
+        options: {
+          animation: {
+            duration: 0,
+          }
+        }
+      };
+
+      new Chart(ctx, chartConfig);
+    }, data);
+
+    const pdfBuffer = await page.pdf();
+
+    await browser.close();
+
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'attachment; filename=generated-pdf.pdf');
+    res.status(200).send(pdfBuffer);
+  } catch (error) {
+    console.error('Error generating PDF:', error);
+    res.status(500).send('Error generating PDF');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
